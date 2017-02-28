8d949cb1af4ba61af10e947e797bf5c423e735e9
Artsiom Marozau <artyom_morozov@outlook.com>
2017-02-28 22:52:33 +0300

    Add simulation tab

--- routes/middleware.js
+++ routes/middleware.js
@@ -21,6 +21,7 @@ exports.initLocals = function (req, res, next) {
 	res.locals.navLinks = [
 		{ label: 'Home', key: 'home', href: '/' },
 		{ label: 'Contact', key: 'contact', href: '/contact' },
+		{ label: 'Simulation', key: 'simulation', href: '/simulation' }
 	];
 	res.locals.user = req.user;
 	next();
