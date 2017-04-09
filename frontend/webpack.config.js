d841a49d448b08968e427f63c1e75ad62b5da957
Valiantsin Haradkou <valicser@yandex.ru>
2017-04-09 21:58:34 +0300

    fix @angular/material version

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -19,7 +19,7 @@ module.exports = {
 
 	output: {
 		path: env.toLowerCase() === 'production' ? '../public/build' : '/build',
-		publicPath: 'http://localhost:8080/',
+		publicPath: 'http://localhost:8880/',
 		filename: '[name].js',
 		chunkFilename: '[id].chunk.js'
 	},
