db4b4546eaa39f805d563320b00963fa90e16718
Valiantsin Haradkou <valicser@yandex.ru>
2017-04-09 23:50:35 +0300

    remove tab

--- frontend/tests/scenarios/simple-scenario.js
+++ frontend/tests/scenarios/simple-scenario.js
@@ -2,7 +2,6 @@
 describe('Protractor Demo App', () => {
     it('should have a title', () => {
         browser.get('http://localhost:8080/');
-        expect(tab).toEqual('a');
         expect(browser.getTitle()).toEqual('Angular With Webpack');
     });
 });
