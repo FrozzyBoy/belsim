569a15acde0264d0c3f909dbf6e2d617aaa2bfd0
Aliaksandra Afanasyeva <azzurues@mail.ru>
2017-04-12 23:28:48 +0200

    Add simple tests for tabs

--- frontend/tests/page-objects/tabsPage.js
+++ frontend/tests/page-objects/tabsPage.js
@@ -7,5 +7,36 @@ module.exports = {
     },
     getTabText: (tabIndex) => {
         return tabsContent.get(tabIndex).getText();
+    },
+    clickAllTabs: () => {
+        return tabs.each(tab => {
+            tab.click();
+            browser.sleep(500);
+        });
+    },
+    getTabsNames: () => {
+        let tabsNames = [];
+
+        return tabs.each(tab => {
+            waitForElementAppearance(tab).then(() => {
+                tab.click().then(() => {
+                    tab.getText().then(text => {
+                        tabsNames.push(text);
+                    });
+                });
+            });
+        }).then(() => {
+            return tabsNames;
+        });
     }
 }
+
+function waitForElementAppearance(element) {
+    let EC = protractor.ExpectedConditions;
+    return browser.wait(EC.presenceOf(element), 5000).then(() => {
+        return browser.wait(() => element.isDisplayed(), 5000).then(() => true);
+    }).catch(() => {
+        console.log("element is not displayed");
+        return false;
+    });
+};
--- frontend/tests/scenarios/simple-scenario.js
+++ frontend/tests/scenarios/simple-scenario.js
@@ -1,8 +1,9 @@
 let tabsPage = require('../page-objects/tabsPage');
+let expectedTabsNames = require('../mock-data/tabs-names');
 
-describe('Protractor Demo App', () => {
-    it('tabs count should be 10', () => {
-        browser.get('http://localhost:8080/');
+describe('check tabs', () => {
+    it('check # of tabs', () => {
+        browser.get('http://localhost:8880/');
 
         let tabsCount = tabsPage.getTabsCount();
         expect(tabsCount).toBe(10);
@@ -11,5 +12,10 @@ describe('Protractor Demo App', () => {
     it('first tab name should be "Производство"', () => {
         let tabName = tabsPage.getTabText(0);
         expect(tabName).toEqual('Производство');
+    });
+
+    it('check tabs names', () => {
+        let tabsNames = tabsPage.getTabsNames();
+        expect(tabsNames).toEqual(expectedTabsNames);
     });
 });
