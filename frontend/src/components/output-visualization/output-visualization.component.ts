4a2ad6fa9a2467759d731741aa889a1ca907324f
Valiantsin Haradkou <valicser@yandex.ru>
2017-04-18 22:47:19 +0300

    added box-plots chart

--- frontend/src/components/output-visualization/output-visualization.component.ts
+++ frontend/src/components/output-visualization/output-visualization.component.ts
@@ -1,24 +1,19 @@
-import {
-  Component,
-  OnInit
-} from '@angular/core';
+import { Component, OnInit, ViewChild, ViewEncapsulation, ElementRef } from '@angular/core';
 
-let drawAll = require('./drawer').drawAll;
+let drawAll = require('./chart/box-plots').drawAll;
 
 import * as d3 from 'd3';
 
 @Component({
   selector: 'output-visualization',
   templateUrl: './output-visualization.tpl.html',
-  styleUrls: ['./output-visualization.component.css']
+  styleUrls: ['./output-visualization.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class OutputVisualizationComponent implements OnInit {
+  @ViewChild('chart') private chart: ElementRef;
+
   public ngOnInit(): void {
-    drawAll();
-    // let containerSelector = "#output-visialization";
-    // initGraph(containerSelector);
+    drawAll(this.chart.nativeElement);
   }
 }
-
-// function initGraph(selector: string) {
-// }
