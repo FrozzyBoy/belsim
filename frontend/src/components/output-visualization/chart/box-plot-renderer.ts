1b4e04cc6445d90d8b8369b0a76de3ad14631dd6
AMarozau <AMarozau@slb.com>
2017-06-12 19:29:09 +0300

    Render chart

--- frontend/src/components/output-visualization/chart/box-plot-renderer.ts
+++ frontend/src/components/output-visualization/chart/box-plot-renderer.ts
@@ -1,8 +1,110 @@
 import * as d3 from 'd3';
+import * as _ from 'underscore';
 
 export class BoxPlotRenderer {
-  public render(): void {
+  public render(
+    bar: d3.Selection<any, {}, null, undefined>,
+    d: { min: number, max: number, median: number, height: number, width: number, minQuarter: number, maxQuarter: number },
+    yScale: d3.ScaleLinear<number, number>
+  ): void {
+    let correctionElementWidth = 10;
+    let xCenter = d.width / 2;
+    let xStart = xCenter / 2;
+    let xEnd = xCenter + xStart;
 
+    this.addAllTextValues(
+      bar,
+      d,
+      xStart,
+      xEnd,
+      yScale
+    );
+
+    this.addQuarter(
+      bar,
+      d,
+      xCenter,
+      yScale
+    );
+
+    let median = bar.append('g')
+      .classed('output-data__chart-median', true);
+
+    let rectHeight = yScale(d.minQuarter) - yScale(d.maxQuarter);
+    median.append('rect')
+      .attr('x', xStart)
+      .attr('y', yScale(d.maxQuarter))
+      .attr('width', xEnd - xStart)
+      .attr('height', rectHeight);
+
+    bar.selectAll('g.text-values')
+    // .append('text')
+    // .attr('transform', y => `translate(${xEnd}, ${yScale(y)})`)
+    // .classed('median-text', true)
+    // .text(x => x)
+    // .attr("font-family", "sans-serif")
+    // .attr("font-size", "8px");
+
+  }
+
+  private addQuarter(
+    bar: d3.Selection<any, {}, null, undefined>,
+    d: { min: number, max: number, median: number, height: number, width: number, minQuarter: number, maxQuarter: number },
+    xCenter: number,
+    yScale: d3.ScaleLinear<number, number>
+  ): void {
+    let quarterData = [{ val: d.max, quarterVal: d.maxQuarter }, { val: d.min, quarterVal: d.minQuarter }];
+    if (d.min === d.max) {
+      quarterData = [{ val: d.max, quarterVal: d.maxQuarter }];
+    }
+
+    bar.selectAll('g.quarter')
+      .data(quarterData)
+      .enter()
+      .append('g')
+      .classed('quarter', true)
+      .append('line')
+      .classed('output-data__chart-line', true)
+      .classed('output-data__chart-line-dotted', true)
+      .attr('y1', x => yScale(x.val))
+      .attr('y2', x => yScale(x.quarterVal))
+      .attr('x1', xCenter)
+      .attr('x2', xCenter);
+  }
+
+  private addAllTextValues(
+    bar: d3.Selection<any, {}, null, undefined>,
+    d: { min: number, max: number, median: number, height: number, width: number, minQuarter: number, maxQuarter: number },
+    xStart: number,
+    xEnd: number,
+    yScale: d3.ScaleLinear<number, number>
+  ): void {
+    let extremumsData: Array<number> = [];
+    if (d.min === d.max) {
+      extremumsData = [d.min];
+    } else {
+      extremumsData = _.values(d);
+    }
+
+    let extremumGroup = bar.selectAll('g.values')
+      .data(extremumsData)
+      .enter()
+      .append('g')
+      .classed('values', true)
+      .attr('transform', y => `translate(0, ${yScale(y)})`);
+
+    extremumGroup.append('line')
+      .classed('output-data__chart-line', true)
+      .attr('y1', 0)
+      .attr('y2', 0)
+      .attr('x1', xStart)
+      .attr('x2', xEnd);
+    extremumGroup.append('text')
+      .classed('median-text', true)
+      .attr('transform', y => `translate(${xEnd}, 0)`)
+      .text(x => x)
+      .attr("font-family", "sans-serif")
+      .attr("font-size", "8px");
   }
 
   public reRender(): void {
--- frontend/src/components/output-visualization/chart/chart-renderer.ts
+++ frontend/src/components/output-visualization/chart/chart-renderer.ts
@@ -1,34 +1,63 @@
 import * as d3 from 'd3';
 import * as _ from 'underscore';
+import { BoxPlotRenderer } from './box-plot-renderer';
+
 
 export class ChartRenderer {
 
+  private boxPlotRenderer: BoxPlotRenderer;
+
+  constructor() {
+    this.boxPlotRenderer = new BoxPlotRenderer();
+  }
+
   public render(element: any, data: { name: string, values: [{ min: number, max: number, median: number }] }): void {
+    let margin = { top: 30, right: 20, bottom: 70, left: 100 };
     let max = _.chain(data.values).max(x => x.max).value().max;
     let min = _.chain(data.values).min(x => x.min).value().min;
-    let height = 480;
-    let width = 1000;
-    let x = d3.scaleBand()
-      .range([0, data.values.length])
-      .padding(.1);
-    let barWidth = x.bandwidth();
+    let height = 480 - margin.top - margin.bottom;
+    let barHeight = height;
+    let width = 1200 - margin.left - margin.right;
+    let deltaCorrection = (Math.abs(max - min) / 2) * 0.1;
+
     let y = d3.scaleLinear()
-      .range([min, max]);
-
+      .domain([max + deltaCorrection, min - deltaCorrection])
+      .range([0, height]);
+    let x = d3.scaleLinear()
+      .domain([0, data.values.length])
+      .range([0, width]);
+    let barWidth = x(1) - x(0);
     let chart = d3.select(element)
-      .attr('width', width)
-      .attr('height', height);
+      .attr('height', height + margin.top + margin.bottom)
+      .attr('width', width + margin.left + margin.right)
+      .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-    let bar = d3.selectAll('g')
+    let bar = chart.selectAll('g')
       .data(data.values)
       .enter()
       .append('g')
-      .attr('transform', (d, i) => `translate(${i * barWidth}, 0)`)
+      .attr('transform', (d, i) => `translate(${x(i)}, 0)`);
 
-    bar.append('rect')
-      .attr('y', d => y(d.median))
-      .attr('height', '10px')
-      .attr('width', '20px');
+    let that = this;
+    bar.each(function (d: any) {
+      d.height = barHeight;
+      d.width = barWidth;
+
+      that.boxPlotRenderer.render(d3.select(this), d, y);
+    });
+
+    let xAxis = d3.axisBottom(x);
+    chart.append('g')
+      .attr('class', 'x axis')
+      .attr('transform', `translate(0, ${height})`)
+      .call(xAxis);
+
+    let yAxis = d3.axisLeft(y);
+    chart.append('g')
+      .attr('class', 'y axis')
+      .call(yAxis);
+
   }
 
   public reRender(): void {
